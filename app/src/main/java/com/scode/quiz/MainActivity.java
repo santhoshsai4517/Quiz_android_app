2e877156880e0e860b1aae6fbda984c25b267661
santhoshsai4517 <santhoshsai4517@gmail.com>
2021-07-07 12:25:01 +0530

    Created forgot password activity layout and linked to login

--- app/src/main/java/com/scode/quiz/MainActivity.java
+++ app/src/main/java/com/scode/quiz/MainActivity.java
@@ -6,10 +6,12 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
     Button register;
+    TextView fpassword;
 
 
     @Override
@@ -22,6 +24,15 @@ public class MainActivity extends AppCompatActivity {
             @Override
             public void onClick(View v) {
                 Intent in = new Intent(MainActivity.this,Register.class);
+                startActivity(in);
+            }
+        });
+
+        fpassword = findViewById(R.id.Forgotpassword);
+        fpassword.setOnClickListener(new View .OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent in = new Intent(MainActivity.this,ForgotPassword.class);
                 startActivity(in);
             }
         });
